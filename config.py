7410e6ff48309f016382d91a4c9c60fce8505eb8
dafa779 <bene567899@gmail.com>
2026-04-13 07:39:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
 SUPPORT_USERNAME = os.getenv("SUPPORT_USERNAME", "@support").strip()
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "@channel").strip()
+PAYMENT_ADDRESS = os.getenv("PAYMENT_ADDRESS", "").strip()
 
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN is missing")
